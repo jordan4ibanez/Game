df43ff9f11b0b56e546b7a7ea0b1ac0b859b3c7c
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-06-15 22:57:36 -0400

    Add in basic timed typing effect

--- user_interface/user_interface.lua
+++ user_interface/user_interface.lua
@@ -7,7 +7,8 @@ local function add_element(element_table)
         position = element_table.position,
         effect = element_table.effect,
         size = element_table.size,
-        color = element_table.color
+        color = element_table.color,
+        speed = element_table.speed
     }
 
     -- don't feel like typing this out over and over
@@ -17,21 +18,40 @@ local function add_element(element_table)
     if element_table.effect == "type" then
         element_pointer.current_char = 0
         element_pointer.timer = 0
+        element_pointer.current_string = ""
     end
-
-
-
 end
 
+-- this mainly processes effects on text
 function update_user_interface(delta)
-    print(delta)
+    -- print(string.sub(ui_table.debug.text, 1, 3))
 
+    for id,data in pairs(ui_table) do
+        if data.effect and data.effect == "type" then
+            data.timer = data.timer + delta
+            if data.timer >= data.speed then
+                data.timer = 0
+                data.current_char = data.current_char + 1
+
+                if data.current_char > string.len(data.text) then
+                    data.current_char = 0
+                end
+                data.current_string = string.sub(data.text, 0, data.current_char)
+            end
+        end
+    end
 end
 
 function render_user_interface()
     for id,data in pairs(ui_table) do
         love.graphics.setColor(data.color[1], data.color[2], data.color[3], 1)
-        love.graphics.print(data.text, data.position.x, data.position.y, 0, data.size, data.size)
+        if not data.effect then
+            love.graphics.print(data.text, data.position.x, data.position.y, 0, data.size, data.size)
+        else
+            if data.effect == "type" then
+                love.graphics.print(data.current_string, data.position.x, data.position.y, 0, data.size, data.size)
+            end
+        end
     end
 end
 
@@ -40,6 +60,7 @@ add_element({
     text = "this is my debug",
     position = {x = 100, y = 100},
     effect = "type",
+    speed = 1,
     size = 1,
     color = {1.0, 1.0, 0.0}
 })
