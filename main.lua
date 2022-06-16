a8f25e746a50a7160d5611a3381b0103d09b2c7c
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-06-16 18:00:13 -0400

    batch loader/randomized playing

--- main.lua
+++ main.lua
@@ -29,7 +29,41 @@ function move_player(axis, modifier)
     end
 end
 
+static_sound_table = {}
+
+local function batch_load_ogg(file_name, number_of_assets)
+
+    -- create new distinct table for sound files
+    static_sound_table[file_name] = {}
+
+    -- automate the number of available assets
+    static_sound_table[file_name].limit = number_of_assets
+
+    -- automate addition into table
+    for i = 1,number_of_assets do
+        static_sound_table[file_name][file_name .. "_" .. i] = love.audio.newSource("sound/" .. file_name .. "_" .. i .. ".ogg", "static")
+    end
+end
+
+-- automates the randomization/playing of sounds
+function play_sound(file_name)
+
+    local sound_pointer = static_sound_table[file_name]
+
+    -- single sound, no need to do excess math
+    if sound_pointer.limit == 1 then
+        sound_pointer[file_name .. "_" .. 1]:play()
+    -- batched sound, randomize selection
+    else
+        local selection = math.ceil( math.random() * 5 )
+        print(selection)
+    end
+end
+
+
 function love.load()
+
+    batch_load_ogg("type", 5)
 
 end
 
